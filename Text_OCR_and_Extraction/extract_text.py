8ec35d2ec27c0da918c9542ac99eef484ff40502
Yatrik <shahyatrik217@gmail.com>
2023-08-26 22:55:05 +0530

    Text Extraction and OCR Version 2 - YATRIK SHAH

--- Text_OCR_and_Extraction/extract_text.py
+++ Text_OCR_and_Extraction/extract_text.py
@@ -1,14 +1,12 @@
 import fitz
 
-input_path = r"D:\Yatrik Shah Backup\Downloads\Non-text-searchable.pdf"
-output_path = r"outputs/ocrmypdf.pdf"
+def extract_text_from_pdf(input_path):
+    pdf = fitz.open(input_path)
+    total_pages = pdf.page_count  # Get the total number of pages in the PDF
 
-pdf = fitz.open(input_path)
-
-txt = ''
-for i in range(total_pages):
-    pg = pdf.load_page(i)    
-    txt += pg.get_text()
+    txt = ''
+    for i in range(total_pages):
+        pg = pdf.load_page(i)
+        txt += pg.get_text()
     return txt
 
-print(txt)
